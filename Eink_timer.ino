a48f0247f1f14ff88746900a156cb3f0b47338c9
yu123an <43343181+yu123an@users.noreply.github.com>
2023-06-21 15:12:58 +0800

    联网超时退出

--- Eink_timer.ino
+++ Eink_timer.ino
@@ -23,7 +23,7 @@ RST REASON
 #include "msg.h"
 #include <ArduinoJson.h>       //JSON解析
 #include <WiFiClientSecure.h>  //https请求
-#include <EEPROM.h>           //EEPROM读写
+#include <EEPROM.h>            //EEPROM读写
 //墨水屏实例
 Epd ePaper;
 //字体列表
@@ -47,16 +47,16 @@ uint16_t epd_height = EPD_HEIGHT;
 uint8_t* framePtr = NULL;  // Pointer for the black frame buffer
 int8_t limit = 5;
 int FullUpdateNum = 0;
-static struct rst_info *rinfo = ESP.getResetInfoPtr();//休眠唤醒信息
+static struct rst_info* rinfo = ESP.getResetInfoPtr();  //休眠唤醒信息
 int RST_reason = rinfo->reason;
 #include "EPD_Support.h"  //墨水屏需要
 //显示变量
 String temp, humidity, windDir, wind, _weather, _date;  // 天气状况
-int Temp_in, Humidity_in;                           // 室内温湿度
-int _day, _hour, _minute, _second;                  // 时间更新
-int update_flag = 0;                                // 更新标志位
-float v_bat = 0;                                    // 电量检测
-String JsonMsg;                                     // json串解析
+int Temp_in, Humidity_in;                               // 室内温湿度
+int _day, _hour, _minute, _second;                      // 时间更新
+int update_flag = 0;                                    // 更新标志位
+float v_bat = 0;                                        // 电量检测
+String JsonMsg;                                         // json串解析
 #define MSG_BUFFER_SIZE (2000)
 char msg[MSG_BUFFER_SIZE];
 StaticJsonDocument<2000> Mqtt_Sub;
@@ -148,6 +148,7 @@ void connect_net() {
   while (WiFi.status() != WL_CONNECTED) {  // 等待连接
     delay(500);
     Serial.print('.');
+    i++;
     if (i > 40) {
       Serial.println("联网失败，结束联网");
       break;
@@ -159,25 +160,25 @@ void connect_net() {
   Serial.print("IP address:\t");
   Serial.println(WiFi.localIP());
 }
-void drawHomePage(){
-    Rtc.Begin();  // DS1307时间读写
+void drawHomePage() {
+  Rtc.Begin();  // DS1307时间读写
   if (!Rtc.GetIsRunning()) {
     Serial.println("RTC was not actively running, starting now");
     Rtc.SetIsRunning(true);
   }
   // 绘制顶部时间栏
   RtcDateTime now = Rtc.GetDateTime();
-  _date = read_eeprom(_date_address,_date_long);
-  temp = read_eeprom(temp_address,temp_long);
-  humidity = read_eeprom(humidity_address,humidity_long);
+  _date = read_eeprom(_date_address, _date_long);
+  temp = read_eeprom(temp_address, temp_long);
+  humidity = read_eeprom(humidity_address, humidity_long);
   get_bat();
-  if((now.Minute() % 10 == 1) || RST_reason == 0){
+  if ((now.Minute() % 10 == 1) || RST_reason == 0) {
     if (ePaper.Init(lut_full_update) != 0) {
-    Serial.print("ePaper init failed");
-    while (1) yield();  // Wait here until re-boot
-  }
-  Serial.println("\r\n ePaper initialisation OK");
-  }else{
+      Serial.print("ePaper init failed");
+      while (1) yield();  // Wait here until re-boot
+    }
+    Serial.println("\r\n ePaper initialisation OK");
+  } else {
     if (ePaper.Init(lut_partial_update) != 0) {
       Serial.print("ePaper init failed");
       while (1) yield();  // Wait here until re-boot
@@ -193,51 +194,50 @@ void drawHomePage(){
   Eink.setFreeFont(Digi);
   Eink.setTextColor(INK);
   Eink.setTextDatum(0);
-  Eink.drawString(String(now.Hour() / 10) + String(now.Hour() % 10) + ":" + String(now.Minute() / 10) + String(now.Minute() % 10) , time_x, time_y);
+  Eink.drawString(String(now.Hour() / 10) + String(now.Hour() % 10) + ":" + String(now.Minute() / 10) + String(now.Minute() % 10), time_x, time_y);
   //Eink.drawRect(time_x, time_y, time_lx, time_ly, INK);
   Eink.setFreeFont(DejaVu_10);
-  Eink.fillRect(classdown_x,classdown_y,classup_lx,classup_ly, PAPER);
-  Eink.drawString("21 22 23 31 32 33 Ll",classdown_x,classdown_y);
-  Eink.drawString("T:" + temp +"/" + humidity + "%",temp_x,temp_y);
-  Eink.drawString("B:" + String(v_bat) + "v",humidity_x,humidity_y);
-  Eink.drawString(_date,date_x + 8,date_y + 10);
+  Eink.fillRect(classdown_x, classdown_y, classup_lx, classup_ly, PAPER);
+  Eink.drawString("21 22 23 31 32 33 Ll", classdown_x, classdown_y);
+  Eink.drawString("T:" + temp + "/" + humidity + "%", temp_x, temp_y);
+  Eink.drawString("B:" + String(v_bat) + "v", humidity_x, humidity_y);
+  Eink.drawString(_date, date_x + 8, date_y + 10);
   Eink.setTextColor(PAPER);
-  Eink.fillRect(classup_x,classup_y,classup_lx,classup_ly, INK);
-  Eink.drawString("A1 A2 B2 B3 C1 C2 C3",classup_x,classup_y);
+  Eink.fillRect(classup_x, classup_y, classup_lx, classup_ly, INK);
+  Eink.drawString("A1 A2 B2 B3 C1 C2 C3", classup_x, classup_y);
   //Eink.drawRect(temp_x, temp_y, temp_lx, temp_ly, INK);
   Eink.drawLine(0, temp_y - 2, 250, temp_y - 2, INK);
   Eink.drawLine(0, classdown_y + classup_ly, 250, classdown_y + classup_ly, INK);
-  Eink.drawLine(time_x + time_lx, classdown_y + classup_ly,time_x + time_lx, temp_y - 2,INK);
+  Eink.drawLine(time_x + time_lx, classdown_y + classup_ly, time_x + time_lx, temp_y - 2, INK);
   Eink.setFreeFont(Orbitron_32);
   Eink.setTextColor(INK);
   Eink.setTextDatum(4);
-  Eink.drawString("136",(250 + time_x + time_lx)/2,time_y + time_ly / 2);
+  Eink.drawString("136", (250 + time_x + time_lx) / 2, time_y + time_ly / 2);
   // Eink.loadFont(siyuan_20);
   // Eink.setCursor(40,80);
   // Eink.print("苹果");
   updateDisplay();
 }
 // 读写EEPROM
-void write_eeprom(int addr,String velue){
+void write_eeprom(int addr, String velue) {
   int lenth = velue.length();
-  for(int a = 0;a<lenth;a++){
-    EEPROM.write(addr + a,velue[a]);
+  for (int a = 0; a < lenth; a++) {
+    EEPROM.write(addr + a, velue[a]);
     //delay(50);
   }
   //EEPROM.commit();
 }
-String read_eeprom(int addr,int lenth){
+String read_eeprom(int addr, int lenth) {
   String Text;
-  for(int a = 0;a<lenth;a++){
-   Text +=  char(EEPROM.read(addr + a));
-   //delay(50);
-   
+  for (int a = 0; a < lenth; a++) {
+    Text += char(EEPROM.read(addr + a));
+    //delay(50);
   }
-  return Text ;
+  return Text;
   Serial.print(Text);
 }
 //获取电量电压
-void get_bat(){
+void get_bat() {
   analogRead(A0);
   double _bat = 0;
   for (int i = 0; i < 8; i++) {
@@ -249,36 +249,35 @@ void get_bat(){
 void setup() {
   Serial.begin(115200);
   EEPROM.begin(512);
-  Wire.begin(5,4);
+  Wire.begin(5, 4);
   Rtc.Begin();  // DS1307时间读写
   if (!Rtc.GetIsRunning()) {
     Serial.println("RTC was not actively running, starting now");
     Rtc.SetIsRunning(true);
   }
   RtcDateTime now = Rtc.GetDateTime();
-   if ((RST_reason == 0) || (now.Minute() == 1 ))
-  {
+  if ((RST_reason == 0) || (now.Minute() == 1)) {
     //EN复位
-  connect_net();//连接网络
-  // 更新时间
-  if (WiFi.status() == WL_CONNECTED) {
-    time_update();
-  //获取天气，日历，倒计时，温湿度
-  get_net(web_hf,1);
-  _date = Mqtt_Sub["updateTime"].as<String>().substring(0,10);
-  temp = Mqtt_Sub["now"]["temp"].as<String>();
-  humidity = Mqtt_Sub["now"]["humidity"].as<String>();
-  write_eeprom(_date_address,_date);
-  write_eeprom(humidity_address,humidity);
-  write_eeprom(temp_address,temp);
-  EEPROM.commit();}
-  }else{
+    connect_net();  //连接网络
+    // 更新时间
+    if (WiFi.status() == WL_CONNECTED) {
+      time_update();
+      //获取天气，日历，倒计时，温湿度
+      get_net(web_hf, 1);
+      _date = Mqtt_Sub["updateTime"].as<String>().substring(0, 10);
+      temp = Mqtt_Sub["now"]["temp"].as<String>();
+      humidity = Mqtt_Sub["now"]["humidity"].as<String>();
+      write_eeprom(_date_address, _date);
+      write_eeprom(humidity_address, humidity);
+      write_eeprom(temp_address, temp);
+      EEPROM.commit();
+    }
+  } else {
     //其他复位源
   }
-drawHomePage();
- ESP.deepSleep((61 - now.Second()) * 1000000);
+  drawHomePage();
+  ESP.deepSleep((61 - now.Second()) * 1000000);
 }
 
 void loop() {
- 
 }
