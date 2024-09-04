d1cc9620061d445ed4a0e4ae9bb44c03bedf7a32
yu123an <43343181+yu123an@users.noreply.github.com>
2024-09-04 22:10:12 +0800

    Create 16_dot.c
    16点阵；加入PCF8563芯片的读写

---
+++ 16_dot.c
@@ -0,0 +1,224 @@
+#include <Arduino.h>
+#include "AiEsp32RotaryEncoder.h"
+#include <msg.h>
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h> // WS2812驱动
+#endif
+#include <Wire.h>
+#include <Rtc_Pcf8563.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+#include <NTPClient.h> //网络校时
+#include <WiFiUdp.h>   //WiFi支持
+#include <EEPROM.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include <ArduinoJson.h>  //JSON解析
+#include <PubSubClient.h> //MQTT
+#include <Ticker.h>       //定时器
+#include "Adafruit_FRAM_I2C.h"
+#define LED_NUM 256
+#define LED 15
+// 编码器
+#define ROTARY_ENCODER_A_PIN 14
+#define ROTARY_ENCODER_B_PIN 12
+#define ROTARY_ENCODER_BUTTON_PIN 13
+#define ROTARY_ENCODER_VCC_PIN -1
+#define ROTARY_ENCODER_STEPS 4
+AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(ROTARY_ENCODER_A_PIN, ROTARY_ENCODER_B_PIN, ROTARY_ENCODER_BUTTON_PIN, ROTARY_ENCODER_VCC_PIN, ROTARY_ENCODER_STEPS);
+// EEPROM地址
+#define EEPROM_ADDR 0x50
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE (25000)
+char msg[MSG_BUFFER_SIZE];
+StaticJsonDocument<25000> Mqtt_Sub; // JSON解析缓存
+int value = 0;
+String JsonMsg;
+int color_r = 255, color_g = 36, color_b = 234;
+int gif_num;
+int light = 10;
+int Sun_rise_hour, Sun_rise_minute, Sun_set_hour, Sun_set_minute;
+// 构造对象
+Adafruit_NeoPixel WS(LED_NUM, LED, NEO_GRB + NEO_KHZ800); // WS2812
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "1.openwrt.pool.ntp.org", 3600 * 8, 60000);
+Rtc_Pcf8563 rtc;
+WiFiClient espClient;
+Ticker time_update;
+Ticker msg_update;
+Ticker ticker;
+Adafruit_EEPROM_I2C i2ceeprom;
+// 编码器相关
+void rotary_onButtonClick()
+{
+  static unsigned long lastTimePressed = 0;
+  // ignore multiple press in that time milliseconds
+  if (millis() - lastTimePressed < 500)
+  {
+    return;
+  }
+  lastTimePressed = millis();
+  Serial.print("button pressed ");
+  Serial.print(millis());
+  Serial.println(" milliseconds after restart");
+}
+void rotary_loop()
+{
+  // dont print anything unless value changed
+  if (rotaryEncoder.encoderChanged())
+  {
+    Serial.print("Value: ");
+    Serial.println(rotaryEncoder.readEncoder());
+  }
+  if (rotaryEncoder.isEncoderButtonClicked())
+  {
+    rotary_onButtonClick();
+  }
+}
+void IRAM_ATTR readEncoderISR()
+{
+  rotaryEncoder.readEncoder_ISR();
+}
+// 读写EEPROM
+void write_eeprom(int addr, String velue)
+{
+  int lenth = velue.length();
+  for (int a = 0; a < lenth; a++)
+  {
+    EEPROM.write(addr + a, velue[a]);
+  }
+}
+String read_eeprom(int addr, int lenth)
+{
+  String Text;
+  for (int a = 0; a < lenth; a++)
+  {
+    Text += char(EEPROM.read(addr + a));
+  }
+  return Text;
+  Serial.print(Text);
+}
+// 联网支持
+void get_net(String web, bool isdecode)
+{
+  HTTPClient http;
+  if (http.begin(espClient, web))
+  {
+    Serial.println("HTTPclient setUp done!");
+  }
+  int httpCode = http.GET();
+  if (httpCode > 0)
+  {
+    if (httpCode == HTTP_CODE_OK)
+    {
+      String payload = http.getString();
+      // Serial.println(payload);
+      if (isdecode)
+      {
+        deserializeJson(Mqtt_Sub, payload);
+      }
+      else
+      {
+        JsonMsg = payload;
+      }
+      // Serial.println(payload);
+    }
+  }
+  else
+  {
+
+    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+  http.end();
+}
+// http字符串解析
+void JsonEecoed(String json)
+{
+  deserializeJson(Mqtt_Sub, json);
+}
+// 主函数
+void setup()
+{
+  EEPROM.begin(200);
+  rtc.initClock();
+  rotaryEncoder.begin();
+  rotaryEncoder.setup(readEncoderISR);
+  bool circleValues = false;
+  rotaryEncoder.setBoundaries(0, 255, circleValues);
+  rotaryEncoder.setAcceleration(250);
+  ticker.attach_ms(500, rotary_loop);
+  Wire.setClock(1000000);
+  WS.begin(); // WS2812驱动使能
+  Wire.begin(5, 4);
+  Serial.begin(115200);
+  if (i2ceeprom.begin(0x50))
+  { // EEProm使能
+    Serial.println("Found I2C EEPROM");
+  }
+  else
+  {
+    Serial.println("I2C EEPROM not identified ... check your connections?\r\n");
+    while (1)
+      delay(10);
+  }
+  WiFi.begin(ssid, password); // 连接网络
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+  while (WiFi.status() != WL_CONNECTED)
+  { // 等待连接
+    delay(500);
+    Serial.print('.');
+  }
+  Serial.println('\n');
+  Serial.println("Connection established!");
+  // get_net();
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());
+  timeClient.begin(); // 获取网络时间
+  timeClient.update();
+  //hr, min, sec
+  rtc.setTime(timeClient.getHours(),timeClient.getMinutes(),timeClient.getSeconds());
+  get_net(gif_msg, 1);
+  gif_num = i2ceeprom.read(0);
+  String gif_name = Mqtt_Sub["name"].as<String>();
+  if (gif_num != Mqtt_Sub["num"].as<int>())
+  {
+    Serial.println("new gif");
+    gif_num = Mqtt_Sub["num"].as<int>();
+    i2ceeprom.write(0, gif_num);
+    for (int num = 0; num < gif_num; num++)
+    {
+      get_net(gif_addr + gif_name + "--" + String(num) + "_bin.log", 1);
+      for (int i = 0; i < 768; i++)
+      {
+        i2ceeprom.write(num * 768 + i + 2, Mqtt_Sub[i]["n"]);
+      }
+    }
+  }
+  else
+  {
+    Serial.println("the same gif");
+  }
+}
+void loop()
+{
+  WS.setBrightness(light);
+  for (int mm = 0; mm < 10; mm++)
+  {
+    int num_gif = i2ceeprom.read(0);
+    for (int num = 0; num < num_gif; num++)
+    {
+      for (int i = 0; i < 256; i++)
+      {
+        WS.setPixelColor(i, i2ceeprom.read(num * 768 + 2 + i * 3 + 2), i2ceeprom.read(num * 768 + 2 + i * 3 + 1), i2ceeprom.read(num * 768 + 2 + i * 3 + 0));
+      }
+      WS.show();
+      //  delay(1000 / num_gif);
+      // ESP.wdtFeed();
+      delay(20);
+    }
+  }
+Serial.println(rtc.getHour());
+  delay(998);
+}
